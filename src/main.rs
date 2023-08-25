e2f128f5947db272f30c416e7c781833d07c1c3a
DegenerateCoder <95440121+DegenerateCoder@users.noreply.github.com>
2023-08-25 17:02:02 +0200

    Add basic implementation of endless music playback based on related streams

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,8 @@ struct MusicPlayer {
     play_video: bool,
     piped_api_domains: Vec<String>,
     piped_api_domain_index: usize,
+    played_video_ids: Vec<String>,
+    duration_limit: u64,
 }
 
 impl MusicPlayer {
@@ -25,6 +27,8 @@ impl MusicPlayer {
             play_video: false,
             piped_api_domains: vec!["https://piped-api.garudalinux.org".to_string()],
             piped_api_domain_index: 0,
+            played_video_ids: Vec::new(),
+            duration_limit: 600,
         }
     }
     fn play(mut self, user_input: &str) {
@@ -40,9 +44,12 @@ impl MusicPlayer {
         }
     }
     fn play_audio(&mut self) {
-        self.url_into_video_id();
+        self.music_to_play = self.url_into_video_id(&self.music_to_play);
+        self.played_video_ids.push(self.music_to_play.clone());
         self.get_audio_url();
-        self.play_music_mpv();
+        loop {
+            self.play_music_mpv();
+        }
     }
 
     fn play_playlist(&self) {
@@ -53,65 +60,30 @@ impl MusicPlayer {
         unimplemented!("audio and video playback");
     }
 
-    fn url_into_video_id(&mut self) {
-        let split = self.music_to_play.split("v=");
-        self.music_to_play = split.last().unwrap().to_string();
-    }
-    fn get_audio_url(&mut self) {
-        let request_url = format!(
-            "{}/streams/{}",
-            self.piped_api_domains[self.piped_api_domain_index], self.music_to_play
-        );
-        let mut response: serde_json::Value = reqwest::blocking::get(&request_url)
-            .unwrap()
-            .json()
-            .unwrap();
-        let audio_streams: &mut Vec<serde_json::Value> = response
-            .get_mut("audioStreams")
-            .take()
-            .unwrap()
-            .as_array_mut()
-            .unwrap();
-        audio_streams.sort_by_key(|x| x.get("bitrate").unwrap().as_u64().unwrap());
-        let audio_stream = audio_streams.last().unwrap();
-        let music_url = audio_stream.get("url").take().unwrap();
-        self.music_to_play = music_url.to_string();
-        println!("{}", music_url);
-        let music_title = response.get("title").take().unwrap();
-        println!("{music_title}");
-        println!("----- ");
-    }
-
     fn play_music_mpv(&mut self) {
         let mpv = libmpv::Mpv::new().unwrap();
-        mpv.set_property("volume", 15).unwrap();
+        mpv.set_property("volume", 100).unwrap();
         mpv.set_property("vo", "null").unwrap();
 
         let mut ev_ctx = mpv.create_event_context();
         ev_ctx.disable_deprecated_events().unwrap();
+        /*
         ev_ctx
             .observe_property("volume", libmpv::Format::Int64, 0)
             .unwrap();
         ev_ctx
             .observe_property("demuxer-cache-state", libmpv::Format::Node, 0)
             .unwrap();
-
+        */
         crossbeam::scope(|scope| {
             scope.spawn(|_| {
-                self.music_to_play = self.music_to_play.replace("\"", "");
-                println!("{}", self.music_to_play);
                 mpv.playlist_load_files(&[(
                     &self.music_to_play,
                     libmpv::FileState::AppendPlay,
                     None,
                 )])
                 .unwrap();
-
-                std::thread::sleep(std::time::Duration::from_secs(3));
-
-                mpv.set_property("volume", 100).unwrap();
-
-                std::thread::sleep(std::time::Duration::from_secs(5));
+                self.prepare_next_to_play();
 
                 // Trigger `Event::EndFile`.
                 //mpv.playlist_next_force().unwrap();
@@ -127,20 +99,106 @@ impl MusicPlayer {
 
                     Ok(libmpv::events::Event::PropertyChange {
                         name: "demuxer-cache-state",
-                        change: libmpv::events::PropertyData::Node(mpv_node),
+                        change: libmpv::events::PropertyData::Node(_mpv_node),
                         ..
                     }) => {
-                        let ranges = seekable_ranges(mpv_node).unwrap();
-                        println!("Seekable ranges updated: {:?}", ranges);
+                        //let ranges = seekable_ranges(mpv_node).unwrap();
+                        //println!("Seekable ranges updated: {:?}", ranges);
                     }
-                    Ok(e) => println!("Event triggered: {:?}", e),
+                    Ok(libmpv::events::Event::StartFile) => {
+                        println!("START");
+                    }
+                    Ok(e) => {
+                        println!("Event triggered: {:?}", e);
+                    }
                     Err(e) => println!("Event errored: {:?}", e),
                 }
             });
         })
         .unwrap();
     }
+
+    fn prepare_next_to_play(&mut self) {
+        self.get_related_video_url();
+        self.music_to_play = self.url_into_video_id(&self.music_to_play);
+        self.played_video_ids.push(self.music_to_play.clone());
+        self.get_audio_url();
+    }
+
+    fn get_audio_url(&mut self) {
+        let request_url = format!(
+            "{}/streams/{}",
+            self.piped_api_domains[self.piped_api_domain_index], self.music_to_play
+        );
+        let mut response: serde_json::Value = reqwest::blocking::get(&request_url)
+            .unwrap()
+            .json()
+            .unwrap();
+        let audio_streams: &mut Vec<serde_json::Value> = response
+            .get_mut("audioStreams")
+            .unwrap()
+            .as_array_mut()
+            .unwrap();
+        audio_streams.sort_by_key(|x| x.get("bitrate").unwrap().as_u64().unwrap());
+        let audio_stream = audio_streams.last().unwrap();
+        let music_url = audio_stream.get("url").unwrap();
+        self.music_to_play = music_url.to_string();
+        self.music_to_play = self.music_to_play.replace("\"", "");
+        let music_title = response.get("title").unwrap();
+        println!("{music_title}");
+        println!("----- ");
+    }
+
+    fn get_related_video_url(&mut self) {
+        let request_url = format!(
+            "{}/streams/{}",
+            self.piped_api_domains[self.piped_api_domain_index],
+            self.played_video_ids.last().unwrap()
+        );
+        let mut response: serde_json::Value = reqwest::blocking::get(&request_url)
+            .unwrap()
+            .json()
+            .unwrap();
+        let related_streams: &mut Vec<serde_json::Value> = response
+            .get_mut("relatedStreams")
+            .unwrap()
+            .as_array_mut()
+            .unwrap();
+
+        for related_stream in related_streams {
+            let related_video_url = related_stream.get("url").unwrap();
+            let related_video_url = related_video_url.as_str().unwrap();
+            if self.check_filters_for_related_video_url(related_video_url, related_stream) {
+                println!("Next to play: {related_video_url}");
+                self.music_to_play = related_video_url.to_string();
+                break;
+            }
+        }
+    }
+
+    fn check_filters_for_related_video_url(
+        &self,
+        video_url: &str,
+        stream_json: &serde_json::Value,
+    ) -> bool {
+        let video_id = self.url_into_video_id(video_url);
+        if self.played_video_ids.contains(&video_id) {
+            return false;
+        } else if stream_json.get("duration").unwrap().as_u64().unwrap() > self.duration_limit {
+            return false;
+        }
+
+        true
+    }
+
+    fn url_into_video_id(&self, url: &str) -> String {
+        let split = url.split("v=");
+        let id = split.last().unwrap().to_string();
+
+        id
+    }
 }
+/*
 
 fn seekable_ranges(demuxer_cache_state: &libmpv::MpvNode) -> Option<Vec<(f64, f64)>> {
     let mut res = Vec::new();
@@ -157,4 +215,4 @@ fn seekable_ranges(demuxer_cache_state: &libmpv::MpvNode) -> Option<Vec<(f64, f6
 
     Some(res)
 }
-
+*/
